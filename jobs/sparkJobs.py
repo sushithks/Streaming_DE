03212d587fb5d203b97c128a76218b6349fe3178
sushith <sushithks@gmail.com>
2025-09-04 13:08:21 -0400

    analysis udf

---
+++ jobs/sparkJobs.py
@@ -0,0 +1,26 @@
+
+import openai
+from config.config import config
+
+
+
+def sentiment_analysis(comment) -> str:
+    if comment:
+        openai.api_key = config['openai']['api_key']
+        completion = openai.ChatCompletion.create(
+            model='gpt-3.5',
+            messages=[
+                {
+                    "role": "system",
+                    "content": """
+                        You're a machine learning model with a task of classifying comments into POSITIVE, NEGATIVE, NEUTRAL.
+                        You are to respond with one word from the option specified above, do not add anything else.
+                        Here is the comment:
+
+                        {comment}
+                    """.format(comment=comment)
+                }
+            ]
+        )
+        return completion.choices[0].message['content']
+    return "Empty Content"
