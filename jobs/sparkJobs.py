038637e6a8e086352adc778f7978cf1583838254
sushith <sushithks@gmail.com>
2025-09-06 18:10:56 -0400

    Stream function

--- jobs/sparkJobs.py
+++ jobs/sparkJobs.py
@@ -1,8 +1,11 @@
+from time import sleep
 
+import pyspark
 import openai
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json, col, when, udf
+from pyspark.sql.types import StructType, StructField, StringType, FloatType
 from config.config import config
-
-
 
 def sentiment_analysis(comment) -> str:
     if comment:
@@ -24,3 +27,36 @@ def sentiment_analysis(comment) -> str:
         )
         return completion.choices[0].message['content']
     return "Empty Content"
+
+
+
+def start_streaming(spark):
+    topic = 'customers_review'
+    while True:
+        try:
+            stream_df = (spark.readStream.format("socket")
+                         .option("host", "0.0.0.0")
+                         .option("port", 9999)
+                         .load()
+                         )
+
+            schema = StructType([
+                StructField("review_id", StringType()),
+                StructField("user_id", StringType()),
+                StructField("business_id", StringType()),
+                StructField("text", StringType())
+            ])
+
+            stream_df = stream_df.select(from_json(col('value'), schema).alias("data")).select(("data.*"))
+
+            sentiment_analysis_udf = udf(sentiment_analysis, StringType())
+
+            stream_df = stream_df.withColumn('feedback',
+                                             when(col('text').isNotNull(), sentiment_analysis_udf(col('text')))
+                                             .otherwise(None)
+                                             )
+
+
+        except Exception as e:
+            print(f'Exception encountered: {e}. Retrying in 10 seconds')
+            sleep(10)
