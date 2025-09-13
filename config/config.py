eb0e3ff62a6a052678f93d85b8578fd810b59df1
sushith <sushithks@gmail.com>
2025-09-13 19:44:09 -0400

    config and docker file update

--- config/config.py
+++ config/config.py
@@ -5,6 +5,8 @@ config = {
     "kafka": {
         "sasl.username": "KAFKA_CLUSTER_API_KEY",
         "sasl.password": "KAFKA_CLUSTER_API_SECRET",
+        "bootstrap.servers": "KAFKA_CLUSTER_BOOTSTRAP_SERVER_URL:PORT",
+        'security.protocol': 'SASL_SSL',
         'sasl.mechanisms': 'PLAIN',
         'session.timeout.ms': 50000
     },
